43f8c6cd633adb97ba5489c7f2013f567c571e75
MishraTanishq619 <mishratanishq619@gmail.com>
2025-01-08 17:38:04 +0530

    Sprint Creation ended

--- app/(main)/project/[projectId]/page.jsx
+++ app/(main)/project/[projectId]/page.jsx
@@ -1,6 +1,7 @@
 import { getProject } from "@/actions/projects";
 import { notFound } from "next/navigation";
 import React from "react";
+import SprintCreationBoard from "../_components/SprintCreationBoard";
 
 const page = async ({ params }) => {
 	const { projectId } = params;
@@ -8,7 +9,23 @@ const page = async ({ params }) => {
 	if (!project) {
 		notFound();
 	}
-	return <div>page</div>;
+	return (
+		<div className="container mx-auto">
+			<SprintCreationBoard
+				projectTitle={project.name}
+				projectId={projectId}
+				projectKey={project.key}
+				sprintKey={project.sprints.length + 1}
+			/>
+
+			{/* Sprint Board */}
+			{project.sprints.length > 0 ? (
+				<></>
+			) : (
+				<>Create a Sprint from button above.</>
+			)}
+		</div>
+	);
 };
 
 export default page;
---
+++ app/(main)/project/_components/SprintCreationBoard.jsx
@@ -0,0 +1,194 @@
+"use client";
+import { createSprint } from "@/actions/sprints";
+import { Button } from "@/components/ui/button";
+import { Card, CardContent } from "@/components/ui/card";
+import { Input } from "@/components/ui/input";
+import {
+	Popover,
+	PopoverContent,
+	PopoverTrigger,
+} from "@/components/ui/popover";
+import useFetch from "@/hooks/use-fetch";
+import { sprintSchema } from "@/lib/validators";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { addDays, format } from "date-fns";
+import { CalendarIcon } from "lucide-react";
+import { useRouter } from "next/navigation";
+import React from "react";
+import { useState } from "react";
+import { Chevron, DayPicker } from "react-day-picker";
+import "react-day-picker/dist/style.css";
+import { Controller, useForm } from "react-hook-form";
+import { toast } from "sonner";
+
+const SprintCreationBoard = ({
+	projectTitle,
+	projectId,
+	projectKey,
+	sprintKey,
+}) => {
+	const [showForm, setShowForm] = useState(false);
+
+	const [dateRange, setDateRange] = useState({
+		from: new Date(),
+		to: addDays(new Date(), 14),
+	});
+
+	const router = useRouter();
+
+	const {
+		register,
+		handleSubmit,
+		formState: { errors },
+		control,
+	} = useForm({
+		resolver: zodResolver(sprintSchema),
+		defaultValues: {
+			name: `${projectKey}${sprintKey}`,
+			startDate: dateRange.from,
+			endDate: dateRange.to,
+		},
+	});
+
+	const { loading: createSprintLoading, fn: createSprintFn } =
+		useFetch(createSprint);
+
+	const onSubmit = async (data) => {
+		await createSprintFn(projectId, {
+			...data,
+			startDate: dateRange.from,
+			endDate: dateRange.to,
+		});
+		setShowForm(false);
+		toast.success("Sprint Created Successfully");
+		router.refresh();
+	};
+
+	return (
+		<div>
+			<div className="flex justify-between">
+				<h1 className="text-5xl font-bold mb-8 gradient-title">
+					{projectTitle}
+				</h1>
+				<Button
+					onClick={() => setShowForm(!showForm)}
+					className="mt-2"
+					variant={showForm ? "destructive" : "default"}
+				>
+					{showForm ? "Cancel" : "Create Sprint"}
+				</Button>
+			</div>
+			{showForm && (
+				<Card className="pt-4 mb-4">
+					<CardContent>
+						<form
+							className="flex gap-4 items-end"
+							onSubmit={handleSubmit(onSubmit)}
+						>
+							<div>
+								<label
+									htmlFor="name"
+									className="block text-sm font-medium mb-1"
+								>
+									Sprint Name
+								</label>
+								<Input
+									id="name"
+									readOnly
+									className="bg-slate-950"
+									{...register("name")}
+								/>
+								{errors.name && (
+									<p className="text-red-500 text-sm mt-1">
+										{errors.name.message}
+									</p>
+								)}
+							</div>
+							<div className="flex-1">
+								<label className="block text-sm font-medium mb-1">
+									Sprint Duration
+								</label>
+								<Controller
+									name="dateRange"
+									control={control}
+									render={({ field }) => (
+										<Popover>
+											<PopoverTrigger asChild>
+												<Button
+													className={`w-full justify-start text-left font-normal bg-slate-950 ${
+														!dateRange &&
+														"text-muted-foreground"
+													}`}
+													variant="outline"
+												>
+													<CalendarIcon className="mr-2 h-4 w-4" />
+													{dateRange.from &&
+													dateRange.to ? (
+														format(
+															dateRange.from,
+															"LLL dd, y"
+														) +
+														" - " +
+														format(
+															dateRange.to,
+															"LLL dd, y"
+														)
+													) : (
+														<span>Pick a date</span>
+													)}
+												</Button>
+											</PopoverTrigger>
+											<PopoverContent
+												className="w-auto bg-slate-900"
+												align="start"
+											>
+												<DayPicker
+													mode="range"
+													selected={dateRange}
+													onSelect={(range) => {
+														if (
+															range.from &&
+															range.to
+														) {
+															setDateRange(range);
+															field.onChange(
+																range
+															);
+														}
+													}}
+													classNames={{
+														Chevron:
+															"fill-blue-500",
+														range_start:
+															"bg-blue-700",
+														range_end:
+															"bg-blue-700",
+														range_middle:
+															"bg-blue-400",
+														day_button:
+															"border-none",
+														today: "border-2 border-blue-700",
+													}}
+												/>
+											</PopoverContent>
+										</Popover>
+									)}
+								/>
+							</div>
+							<Button
+								type="submit"
+								disabled={createSprintLoading}
+							>
+								{createSprintLoading
+									? "Creating..."
+									: "Create Sprint"}
+							</Button>
+						</form>
+					</CardContent>
+				</Card>
+			)}
+		</div>
+	);
+};
+
+export default SprintCreationBoard;
